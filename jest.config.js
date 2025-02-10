af8fc117ad75c981d61d8767509a4918264cd314
vitaliy <nertis44@gmail.com>
2025-02-10 21:05:44 +0200

    Jest testing

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true, // в момент запуск теста будет предлагаться больше информации еслим что-то не так
+};
+
+module.exports = config;
